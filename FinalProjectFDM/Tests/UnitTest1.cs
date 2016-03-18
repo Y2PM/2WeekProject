329a728b0cf7d79ad545a632f9680e2e144694ff
cemal13 <cemalsc@gmail.com>
2016-03-18 12:54:11 +0000

    Renamed Tests Project

---
+++ FinalProjectFDM/Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalProjectFDM;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGetOneTenReturnsAnIntFromBetweenOneAndTen()
+        {
+            //Arrange
+            DecathonGame decgame = new DecathonGame();
+
+            //Act
+            int thenumber = decgame.GetOneTen();
+
+            //Assert
+
+        }
+    }
+}
