0b17500f52143d341842043f08374cbc9ffce3e1
Lt. <adanmadavid@hotmail.com>
2016-03-18 11:10:46 +0000

    Added test project and 1st test

---
+++ FinalProjectFDM/FinalProjectTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FinalProjectTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGetOneTenReturnsAnIntFromBetweenOneAndTen()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+
+        }
+    }
+}
