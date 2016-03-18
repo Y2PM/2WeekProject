95d1aeaeb12780baa74957839ec56260b836b976
Lt. <adanmadavid@hotmail.com>
2016-03-18 10:51:58 +0000

    SQL added to solution

---
+++ FinalProjectFDM/GroupProjectSQLQuery.sql
@@ -0,0 +1,25 @@
+﻿IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Game]') AND type in (N'U'))
+DROP TABLE [dbo].[Game]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Member]') AND type in (N'U'))
+DROP TABLE [dbo].[Member]
+
+CREATE TABLE Game
+(
+game_id INT IDENTITY(1,1) PRIMARY KEY,
+name VARCHAR(50),
+payout DECIMAL
+);
+
+CREATE TABLE Member
+(
+member_id INT IDENTITY(1,1) PRIMARY KEY,
+m_username VARCHAR(50),
+m_password VARCHAR(50),
+m_account DECIMAL
+);
+
+INSERT INTO Game (name) VALUES ('Decathon');
+INSERT INTO Game (payout) VALUES (10);
+INSERT INTO Member (m_username) VALUES ('ragingbull');
+INSERT INTO Member (m_password) VALUES ('password123');
+INSERT INTO Member (m_account) VALUES (0.0);
