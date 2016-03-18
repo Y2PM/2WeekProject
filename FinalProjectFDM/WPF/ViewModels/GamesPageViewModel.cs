da29881c15984cc6869cfc52d69ab92829612bf8
cemal13 <cemalsc@gmail.com>
2016-03-18 15:34:32 +0000

    fix

---
+++ FinalProjectFDM/WPF/ViewModels/GamesPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF.ViewModels
+{
+    public class GamesPageViewModel : BaseViewModel
+    {
+        private ICommand _navigateToMainPageCommand;
+
+        public ICommand navigateToHomePageCommand
+        {
+            get
+            {
+                if (_navigateToMainPageCommand == null)
+                {
+                    _navigateToMainPageCommand = new Command(navigateToMainPage, canNavigateToMainPage);
+                }
+                return _navigateToMainPageCommand;
+            }
+            set { _navigateToMainPageCommand = value; }
+        }
+
+        public bool canNavigateToMainPage()
+        {
+            return true;
+        }
+
+        public void navigateToMainPage()
+        {
+            MainWindowViewModel windowViewModel = App.Current.MainWindow.DataContext as MainWindowViewModel;
+            windowViewModel.page = "MainPage.xaml";
+        }
+    }
+}
---
+++ FinalProjectFDM/WPF/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private ICommand _navigateToGamesPageCommand;
+        public ICommand navigateToGamesPageCommand
+        {
+            get
+            {
+                if (_navigateToGamesPageCommand == null)
+                {
+                    _navigateToGamesPageCommand = new Command(navigateToGamesPage, canNavigateToGamesPage);
+                }
+                return _navigateToGamesPageCommand;
+            }
+            set { _navigateToGamesPageCommand = value; }
+        }
+
+        public bool canNavigateToGamesPage()
+        {
+            return true;
+        }
+
+        public void navigateToGamesPage()
+        {
+            MainWindowViewModel windowViewModel = App.Current.MainWindow.DataContext as MainWindowViewModel;
+            windowViewModel.page = "GamesPage.xaml";
+        }
+    }
+}
