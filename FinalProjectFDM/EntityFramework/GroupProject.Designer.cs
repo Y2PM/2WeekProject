986c081d31fe4672f90e05bc4abd3279ebc5d15b
cemal13 <cemalsc@gmail.com>
2016-03-18 13:31:32 +0000

    updated entityframework

--- FinalProjectFDM/EntityFramework/GroupProject.Designer.cs
+++ FinalProjectFDM/EntityFramework/GroupProject.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\cemal.coskun\Source\Repos\2WeekProject\FinalProjectFDM\EntityFramework\FinalProject.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\cemal.coskun\Source\Repos\2WeekProject\FinalProjectFDM\EntityFramework\GroupProject.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
