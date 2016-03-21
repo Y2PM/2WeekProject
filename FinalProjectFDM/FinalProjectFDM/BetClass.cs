ca5d4fc665e804241a3dc30eef405e0c18f00184
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-21 08:23:49 +0000

    betclass

---
+++ FinalProjectFDM/FinalProjectFDM/BetClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectFDM
+{
+    public class BetClass
+    {
+        public string Bet(double numberBid, double moneyGiven)
+        {
+            if (numberBid > 1 || numberBid < 0)
+            {
+                return "invalid input";
+            }
+
+
+
+            return "0";
+        }
+    }
+}
