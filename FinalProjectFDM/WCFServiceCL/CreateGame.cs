d289ceb8f7719a32b168ba0a96d227481ba5bea7
Y2PM2 <joseph.bannert@fdmgroup.com>
2016-03-21 16:54:22 +0000

    changes

--- FinalProjectFDM/WCFServiceCL/CreateGame.cs
+++ FinalProjectFDM/WCFServiceCL/CreateGame.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WCFServiceCL
 {
-    public class Class1
+    [DataContract]
+    public class CreateGame
     {
+        [DataMember]
+
     }
 }
